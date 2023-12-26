ace317695b9b3c9ef9596709605435a655f88011
Krishna Chaitanya A <1113akc@gmail.com>
2023-12-26 17:18:40 +0530

    Phase 1 Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClientCreationComponent } from './client-creation/client-creation.component';
+import { ClientMeetingsComponent } from './client-meetings/client-meetings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },  // Make sure this line is present
+  { path: 'client-creation', component: ClientCreationComponent },
+  { path: 'client-meetings', component: ClientMeetingsComponent },
+  { path: '**', redirectTo: '' } // Redirect any unknown paths to ''
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// app.module.ts
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientCreationComponent } from './client-creation/client-creation.component';
+import { ClientMeetingsComponent } from './client-meetings/client-meetings.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientCreationComponent,
+    ClientMeetingsComponent,
+    HomeComponent,
+    
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule], // Add HttpClientModule here
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
